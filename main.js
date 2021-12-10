3ca4694b07ccab6aca2094adc900da4ff0d35f9b
shivkar2n <shivkar2n@gmail.com>
2021-12-10 13:04:30 +0530

    Made simple wave animation

---
+++ main.js
@@ -0,0 +1,119 @@
+anime({
+    targets: 'div.column.one', // HTML element to apply animation
+    height: [
+        { value: '75%', duration: 400 },
+        { value: '40%', duration: 800 },
+    ],
+
+    // Change height from px to 100%
+    easing: 'easeInOutQuad', // Define timing function of animation
+    direction: 'alternate', // Direction of animation alternates
+    loop: true
+});
+
+anime({
+    targets: 'div.column.two',
+    height: [
+        { value: '75%', duration: 450 },
+        { value: '40%', duration: 750 },
+    ],
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: true
+});
+
+anime({
+    targets: 'div.column.three',
+    height: [
+        { value: '75%', duration: 500 },
+        { value: '40%', duration: 700 },
+    ],
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: true
+});
+
+anime({
+    targets: 'div.column.four',
+    height: [
+        { value: '75%', duration: 550 },
+        { value: '40%', duration: 650 },
+    ],
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: true
+
+});
+
+anime({
+    targets: 'div.column.five',
+    height: [
+        { value: '75%', duration: 600 },
+        { value: '40%', duration: 600 },
+    ],
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: true
+
+});
+
+anime({
+    targets: 'div.column.six',
+    height: [
+        { value: '75%', duration: 650 },
+        { value: '40%', duration: 550 },
+    ],
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: true
+
+});
+
+
+anime({
+    targets: 'div.column.seven',
+    height: [
+        { value: '75%', duration: 700 },
+        { value: '40%', duration: 500 },
+    ],
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: true
+
+});
+
+anime({
+    targets: 'div.column.eight',
+    height: [
+        { value: '75%', duration: 750 },
+        { value: '40%', duration: 450 },
+    ],
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: true
+
+});
+
+anime({
+    targets: 'div.column.nine',
+    height: [
+        { value: '75%', duration: 800 },
+        { value: '40%', duration: 400 },
+    ],
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: true
+
+});
+
+anime({
+    targets: 'div.column.ten',
+    height: [
+        { value: '75%', duration: 850 },
+        { value: '40%', duration: 350 },
+    ],
+    easing: 'easeInOutQuad',
+    direction: 'alternate',
+    loop: true
+
+});
