d8c7e9103afb6777da4cb86c34c9e95cf58857af
shivkar2n <shivkar2n@gmail.com>
2021-12-10 13:40:37 +0530

    Added play/pause button

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-anime({
+var waveAnimation = anime({
     targets: 'div.column', // HTML element to apply animation
     height: [
         { value: '75%', duration: anime.stagger(50, { start: 400 }) },
@@ -10,3 +10,9 @@ anime({
     direction: 'alternate', // Direction of animation alternates
     loop: true
 });
+
+// console.log(document.getElementsByClassName('play'));
+document.getElementById('playAnim').onclick = () => waveAnimation.play();
+document.getElementById('pauseAnim').onclick = () => waveAnimation.pause();
+
+// document.getElementById('pauseButton').onclick = () => console.log("Hello");
